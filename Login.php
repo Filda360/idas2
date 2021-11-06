f0840892ce934f18f9efdfb0da451d7f7141eb4b
Filda360 <Filda360@seznam.cz>
2021-11-06 21:52:13 +0100

    chyba oci_connect

--- Login.php
+++ Login.php
@@ -1,1 +1,24 @@
-<?php hjvhj
+<?php
+    if(isset($_POST['username'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if(empty($username) && empty($password)){
+            echo "heslo nebo uživatelské jméno nebylo zadané";
+        }else{
+            $conn = oci_connect('ST61021', 'Flynn177', 'fei-sql1.upceucebny.cz/IDAS');
+            if (!$conn) {
+                $e = oci_error();
+                trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+            }
+
+        }
+    }
+
+?>
+
+<form method="POST">
+    <input type="text" name="username" placeholder="Username"><br/>
+    <input type="password" name="password" placeholder="Password"><br/>
+    <button type="submit">Login</button>
+</form>
